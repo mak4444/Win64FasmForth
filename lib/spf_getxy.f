a2b11842af063d4b9d9a2336f74df366a21eb4ca
mak4444 <fpauk@mail.ru>
2022-06-18 22:31:20 +0300

    Add files via upload

--- lib/spf_getxy.f
+++ lib/spf_getxy.f
@@ -1,27 +1,40 @@
-REQUIRE XAPI_2: lib\WAPI.4
+REQUIRE API_2: lib\WAPI.4
 
 KERNEL32DLL XAPI_2: GetConsoleScreenBufferInfo GetConsoleScreenBufferInfo
 KERNEL32DLL XAPI_2: SetConsoleCursorPosition SetConsoleCursorPosition
+
+CREATE CONSOLE_SCREEN_BUFFER_INFO 22 ALLOT
+: SBI CONSOLE_SCREEN_BUFFER_INFO 20 DUMP ;
+
 
 : COORD ( x y -- COORD )
   16 LSHIFT OR 
 ;
 
-: AT-XY ( x y -- )
+: AT-XY-G ( x y -- )
 \ 
   COORD H-STDOUT
   SetConsoleCursorPosition DROP \ ERR THROW
 ;
 
+: AT-XY ( x y -- )
+  CONSOLE_SCREEN_BUFFER_INFO H-STDOUT
+  GetConsoleScreenBufferInfo  DROP
+  CONSOLE_SCREEN_BUFFER_INFO $C + W@ +
+
+ AT-XY-G
+;
+
 : SETXY AT-XY ;
 
-CREATE CONSOLE_SCREEN_BUFFER_INFO 22 ALLOT
-: SBI CONSOLE_SCREEN_BUFFER_INFO 20 DUMP ;
 
 : AT-XY? ( -- x y )
 \ 
   CONSOLE_SCREEN_BUFFER_INFO H-STDOUT
   GetConsoleScreenBufferInfo  DROP
-  CONSOLE_SCREEN_BUFFER_INFO 4 + DUP W@ SWAP 2 + W@ ;
+  CONSOLE_SCREEN_BUFFER_INFO 4 +
+  DUP W@
+  OVER 2 + W@ 
+ ROT 8 + W@ - ;
 
 : GETXY AT-XY? ;
