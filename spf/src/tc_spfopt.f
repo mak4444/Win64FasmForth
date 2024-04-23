b48d7884487914634ab4b3e5110c17c7730c7ac3
mak4444 <fpauk@mail.ru>
2024-04-23 15:49:11 +0300

    Update tc_spfopt.f

--- spf/src/tc_spfopt.f
+++ spf/src/tc_spfopt.f
@@ -7,10 +7,12 @@
 
 : _QVALUE-CODE >R 4 + 5 + @ ;
 : _QTOVALUE-CODE >R  ! ;
+: ABORT<'> ABORT ;
 
 ' _QVALUE-CODE VALUE QVALUE-CODE
 ' _QTOVALUE-CODE VALUE QTOVALUE-CODE
 ' _VALUE-CODE  VALUE VALUE-CODE
+' ABORT<'>  VALUE '<'>
 
 REQUIRE GTYPE _mak/djgpp/gdis.f
 
@@ -170,6 +172,70 @@ DIS-OPT
   DP @ \ DUP TO :-SET
 ; IMMEDIATE
 
+: ?PAIRS        \ x1 x1 --
+  XOR IF -22 THROW THEN ;
+
+3 CONSTANT DO_FLAG1
+
+: L, , ;
+: Q,  , 0 , ;
+
+: DO            \ Run: n1|u1 n2|u2 -- ; R: -- loop-sys           6.1.1240
+\ *G Begin a *\fo{DO ... LOOP} construct. Takes the end-value and
+\ ** start-value from the stack.
+ ['] YDO _COMPILE,  HERE 0 Q, HERE DO_FLAG1
+; IMMEDIATE
+
+: ?DO           \ Run: n1|u1 n2|u2 -- ; R: -- | loop-sys ; 6.2.0620
+\ *G Compile a *\fo{DO} which will only begin loop execution if
+\ ** the loop parameters do not specify an interation count of 0.
+ ['] YQDO _COMPILE, HERE 0 Q, HERE DO_FLAG1
+; IMMEDIATE
+
+: LOOP          \ Run: -- ; R: loop-sys1 -- | loop-sys2         6.1.1800
+\ *G The closing statement of a *\fo{DO ... LOOP} construct.
+\ ** Increments the index and terminates when the index crosses
+\ ** the limit.
+  DO_FLAG1 ?PAIRS
+ 0x49 C, 0xC6FF W, \ inc r14
+ 0x49 C, 0xC7FF W, \ inc r15
+  HERE 2+ -  DUP SHORT? \  SetOP SetJP
+  IF
+    0x71 C, C, \ jno short 
+  ELSE
+    4 - 0xF C, 0x81 C, L, \ jno near
+  THEN  \ SetOP
+  0x5E41 W, \ pop r14
+  0x5F41 W, \ pop r15
+  0x59 C, \ pop rcx
+  HERE SWAP !
+ ; IMMEDIATE
+
+: +LOOP         \ Run: n -- ; R: loop-sys1 -- | loop-sys2       6.1.0140
+\ *G As *\fo{LOOP} except that you specify the increment on the
+\ ** stack. The action of *\fo{n +LOOP} is peculiar when n is
+\ ** negative:
+\ *C   -1 0 ?DO  i .  -1 +LOOP
+\ *P prints *\fo{0 -1}, whereas:
+\ *C   0 0 ?DO  i .  -1 +LOOP
+\ *P prints nothing. This a result of the mathematical trick used
+\ ** to detect the terminating condition. To prevent confusion
+\ ** avoid using *\fo{n +LOOP} with negative *\i{n}.
+  DO_FLAG1 ?PAIRS
+	0x49 C, 0xC601 W, OPT	\   	add    %rax,%r14
+	0x49 C, 0xC701 W, OPT	\	add    %rax,%r15
+     	['] DROP _COMPILE,
+  HERE 2+ -  DUP SHORT? \  SetOP SetJP
+  IF
+    0x71 C, C, \ jno short 
+  ELSE
+    4 - 0xF C, 0x81 C, L, \ jno near
+  THEN  \ SetOP
+  0x5E41 W, \ pop r14
+  0x5F41 W, \ pop r15
+  0x59 C, \ pop rcx
+  HERE SWAP !
+ ; IMMEDIATE
 
 : I   \ 94
   R> 2R@  - SWAP >R ;
@@ -255,9 +321,6 @@ USER-VALUE
 : VARIABLE  VARIABLE 0 , ;
 
 
-
-: L, , ;
-: Q,  , 0 , ;
 
 : VALUE 
   HEADER  QVALUE-CODE 
